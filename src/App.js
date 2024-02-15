7af13c5a86731ee6aa7d6f50e4565fd9d0f93776
UnitySquare <UnitySquare@users.noreply.github.com>
2024-02-15 22:30:36 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
+  // const { unityProvider } = useUnityContext({
+  //   loaderUrl: "unity-local/build/ball-sim.loader.js",
+  //   dataUrl: "unity-local/build/ball-sim.data",
+  //   frameworkUrl: "unity-local/build/ball-sim.framework.js",
+  //   codeUrl: "unity-local/build/ball-sim.wasm",
+  // });
+  
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/ball-sim.loader.js",
+    dataUrl: "build/ball-sim.data",
+    frameworkUrl: "build/ball-sim.framework.js",
+    codeUrl: "build/ball-sim.wasm",
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          하이요!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>react / unity</h1>
+        <Unity unityProvider={unityProvider} style={{ width: "1688px", height: "780px" }}/>
       </header>
     </div>
   );
